a07d82959b56a1ce1d7799b9575b8b0d5639b54f
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-12-05 13:35:02 +0900

    Test: 구입 금액 유효성 검사 테스트 추가

--- __tests__/validators/PurchaseAmountValidatorTest.js
+++ __tests__/validators/PurchaseAmountValidatorTest.js
@@ -8,6 +8,16 @@ describe('구매 금액 예외 상황 테스트', () => {
       description: '아무값도 입력하지 않을 경우 예외처리를 한다.',
       expected: ERROR.purchaseAmount.empty,
     },
+    {
+      input: 'a',
+      description: '숫자가 아닌 값을 입력했을 경우 예외처리를 한다.',
+      expected: ERROR.purchaseAmount.notANumber,
+    },
+    {
+      input: '2300',
+      description: '1,000단위의 구매 금액을 입력하지 않았을 경우 예외처리를 한다.',
+      expected: ERROR.purchaseAmount.multipleOfThousnad,
+    },
   ];
 
   test.each(cases)('구매 금액 $input을 통해 에러를 반환한다.', ({ input, expected }) => {
