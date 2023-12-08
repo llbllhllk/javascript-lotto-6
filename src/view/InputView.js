f38c9b31c43d8e9c4acaf0e91adbe0db248a6bf2
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-12-08 20:44:19 +0900

    Feat: reTry 기능 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,28 +3,34 @@ import MESSAGE from '../constants/message.js';
 import PurchaseAmountValidator from '../utils/validators/PurchaseAmountValidator.js';
 import WinningNumbersValidator from '../utils/validators/WinningNumbersValidator.js';
 import BonusNumberValidator from '../utils/validators/BonusNumberValidator.js';
-import CONSTANTS from '../constants/constants.js';
+import reTry from '../utils/retry.js';
 
 const InputView = {
   async readPurchaseAmount() {
-    const purchaseAmount = await Console.readLineAsync(MESSAGE.read.purchaseAmount);
-    PurchaseAmountValidator.validatePurchaseAmount(purchaseAmount);
+    return reTry(async () => {
+      const purchaseAmount = await Console.readLineAsync(MESSAGE.read.purchaseAmount);
+      PurchaseAmountValidator.validatePurchaseAmount(purchaseAmount);
 
-    return purchaseAmount;
+      return purchaseAmount;
+    });
   },
 
   async readWinningNumbers() {
-    const winningNumbers = await Console.readLineAsync(MESSAGE.read.winningNumbers);
-    WinningNumbersValidator.validateWinningrNumbers(winningNumbers);
+    return reTry(async () => {
+      const winningNumbers = await Console.readLineAsync(MESSAGE.read.winningNumbers);
+      WinningNumbersValidator.validateWinningrNumbers(winningNumbers);
 
-    return winningNumbers;
+      return winningNumbers;
+    });
   },
 
   async readBonusNumber() {
-    const bonusNumber = await Console.readLineAsync(MESSAGE.read.bonusNumber);
-    BonusNumberValidator.validateBonusNumber(bonusNumber);
+    return reTry(async () => {
+      const bonusNumber = await Console.readLineAsync(MESSAGE.read.bonusNumber);
+      BonusNumberValidator.validateBonusNumber(bonusNumber);
 
-    return bonusNumber;
+      return bonusNumber;
+    });
   },
 };
 
